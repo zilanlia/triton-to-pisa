8ac4e0651f9868f69eb7b9adfcbad4d9b9eebd4e
sijialou <sijia.lou@intel.com>
2024-02-01 11:28:08 +0800

    add helper function

---
+++ helper.py
@@ -0,0 +1,19 @@
+import torch
+
+def rand_strided(
+    size,
+    stride,
+    dtype,
+    device,
+    extra_size: int = 0,
+):
+    needed_size = (
+        sum((shape - 1) * stride for shape, stride in zip(size, stride))
+        + 1
+        + extra_size
+    )
+    if dtype.is_floating_point:
+        buffer = torch.randn(needed_size, dtype=dtype, device=device)
+    else:
+        buffer = torch.zeros(size=[needed_size], dtype=dtype, device=device)
+    return torch.as_strided(buffer, size, stride)
